70810e7ba951c9f3e7169431ebab9ae13ce09b14
Peter Dolan <peter@nunahealth.com>
2016-10-06 12:22:55 -0700

    Fix Mac OSX support
    The name of the shared library needs to end in ".dylib" on Mac.
    Previously, it was hardcoded to ".so".

--- src/EGM96.jl
+++ src/EGM96.jl
@@ -1,6 +1,11 @@
-include("../deps/deps.jl")
-
 module EGM96
-  export undulation
-  include("f477.jl")
+
+    if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+        include("../deps/deps.jl")
+    else
+        error("EGM96 not properly installed. Please run Pkg.build(\"EGM96\")")
+    end
+    
+    export undulation
+    include("f477.jl")
 end
